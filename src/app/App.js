1338cea9de0401f008dbecc390d981f21390b3d4
Johan Boll <bollj@cs.uw.edu>
2020-10-17 15:51:41 -0700

    Added ComponentGrid files and test in App

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import ComponentGrid from "../components/ComponentGrid";
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      Working
-    </div>
-  );
+    var aList = [];
+    aList[0] = ("Carousel1");
+    aList[1] = ("Carousel2");
+    aList[2] = ("Carousel3");
+    return (
+        <div>
+            <ComponentGrid carousels={aList} hi={"hello"}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/ComponentGrid.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import './ComponentGrid.css';
+
+class ComponentGrid extends Component {
+    render() {
+        var listOfCarousels = [];
+        for (let i = 0; i < this.props.carousels.length; i++) {
+            listOfCarousels[i] = (
+                <div className={"Carousel"}>
+                    {this.props.carousels[i]}
+                </div>
+            );
+        }
+        return (
+            <div>
+                {listOfCarousels}
+            </div>
+        );
+    }
+}
+
+export default ComponentGrid;
