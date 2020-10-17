539267497e996b2986cbf19db26524ab8dc08ef2
warmuc <warmuc@uw.edu>
2020-10-17 15:54:00 -0700

    Added Search bar styling

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Searchbar from "../components/Searchbar";
 
-function App() {
-  return (
-    <div>
-      <Searchbar/>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchString: ""
+        }
+    }
+
+    updateSearch = (value) => {
+        this.setState({
+            searchString: value
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <Searchbar onChange={this.updateSearch}/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,30 +1,63 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField'
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/styles';
+import './Searchbar.css'
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(1),
-            width: '25ch',
-        },
+const styles = theme => ({
+    textField: {
+        width: '90%',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        paddingBottom: 0,
+        marginTop: 0,
+        fontWeight: 500
     },
-}));
+    input: {
+        color: 'black'
+    }
+});
 
 
 class Searchbar extends Component {
     constructor(props) {
         super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
     }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+        this.props.onChange(event.target.value);
+    };
+
+
 
 
     render() {
+        const { classes } = this.props;
         return (
-            <form className={"search"} noValidate autoComplete="off">
-                <TextField id="searchbar" label="Search" variant="outlined" />
+            <form className={classes.root} noValidate autoComplete="off">
+                <TextField id="searchbar"
+                           label="Search"
+                           variant="outlined"
+                           fullWidth
+                           margin={"normal"}
+                           value={this.state.value}
+                           onChange={this.handleChange}
+                           InputProps={{
+                               className: classes.input,
+                           }}
+                />
             </form>
-        );
+        )
     }
 }
 
-export default Searchbar;
+Searchbar.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+
+export default withStyles(styles)(Searchbar);
