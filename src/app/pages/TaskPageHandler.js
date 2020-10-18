797dbdc0d9fcf75c769ba44382f576b20e94623e
Johan Boll <bollj@cs.uw.edu>
2020-10-17 22:33:28 -0700

    Added simple json database and made small style changes

--- src/app/pages/TaskPageHandler.js
+++ src/app/pages/TaskPageHandler.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import { Link as RouterLink} from 'react-router-dom';
+//import { makeStyles } from '@material-ui/core/styles';
+//import Card from '@material-ui/core/Card';
+//import { Link as RouterLink} from 'react-router-dom';
 import TaskPage from './TaskPage.js';
 import NotFound from './NotFound.js';
+import ActivitiesData from "../../data/ActivitiesData.json"
 
 const TaskPageHandler = (props) => {
   let activityId = parseInt(props.routerProps.match.params.id);
-  let isValid = activityId < 100;
+  let activities = ActivitiesData.activities;
+  let isValid = activityId < activities.length;
+  let thisActivity = activities[activityId];
 
   return (
     <div>
-        { isValid ? <TaskPage id={activityId} /> : <NotFound /> }
+        { isValid ? <TaskPage id={activityId} title={thisActivity.title} description={thisActivity.description}/> : <NotFound /> }
     </div>
   );
 }
